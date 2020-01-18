6b696922e85c53db9b1595c6be640cb45147b914
jrappine <rappine@live.fr>
2020-01-18 13:05:18 +0100

    Modification du repo/ajout de PS

---
+++ PS/disable_admin_shared.ps1
@@ -0,0 +1,21 @@
+﻿##################################################
+#                                                #
+# Désactiver le partage administratif Windows 10 #
+#                                                #
+##################################################
+
+
+##### Stopper et désactiver le service ####
+
+get-service -Name lanmanserver | stop-service -Force
+
+##### Désactiver le service au démarrage #####
+
+set-service -Name lanmanserver -StartupType Disabled
+
+
+##### Désactiver dans le registre #####
+
+$RegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+
+new-itemproperty -Path $RegistryPath -Name "AutoShareWks" -Property "Dword" -Value 0
